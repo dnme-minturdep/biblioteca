2b91822b0c44b39d28fe4165083b67ceb9cfe0a7
Juan Pablo Ruiz Nicolini <jruiznicolini@turismo.gob.ar>
2022-07-11 12:53:52 -0300

    modify layout

--- content/recursos.R
+++ content/recursos.R
@@ -4,19 +4,24 @@ library(tidyverse)
 
 data.frame(
   recurso = c("Turismo Naturaleza",
-              "Anuario 2015"),
+              "Anuario 2015",
+              "Turismo Cultura"),
   src = c("img/perfil_naturaleza.png",
-          "img/aet_2015.png"),
-  url = c("content/files/perfil_naturaleza.pdf",
-          "https://www.yvera.tur.ar/sinta/informe/documentos/descarga/59f0080a467fb.pdf"),
-  description  = c("El reporte brinda información estadística relacionada con las
+          "img/aet_2015.png",
+          "img/perfil_cultura.png"),
+  url = c("https://tableros.yvera.tur.ar/recursos/biblioteca/perfil_naturaleza.pdf",
+          "https://www.yvera.tur.ar/sinta/informe/documentos/descarga/59f0080a467fb.pdf",
+          "https://tableros.yvera.tur.ar/recursos/biblioteca/perfilcultura.pdf"),
+  description  = c("Información estadística relacionada con las
 prácticas turísticas en entornos naturales",
-                   "Anuario de Estadísticas de Turismo 2015 (último publicado)"),
-  tags = c("Reporte", "Anuario"),
-  Todos = c(1, 1),
-  Anuario = c(0, 1),
-  Informe = c(1, 0),
-  Documento = c(0, 0)) %>%
+                   "Anuario de Estadísticas de Turismo 2015 (último publicado)",
+                   "Información estadística relacionada con las
+prácticas turísticas en entornos culturales"),
+  tags = c("Reporte", "Anuario", "Reporte"),
+  Todos = c(1, 1, 1),
+  Anuario = c(0, 1, 0),
+  Informe = c(1, 0, 1),
+  Documento = c(0, 0, 0)) %>%
   as_tibble() %>%
   mutate(orden = c(1)) %>%   # OREDNO POR TEMA Y NO POR TIPO DE RECURSO
   arrange(orden)  %>%
