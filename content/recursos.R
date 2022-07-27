a64261e318ddbad99459b14302af3c3bc49e26b7
Juan Pablo Ruiz Nicolini <juanpabloruiznicolini@gmail.com>
2022-07-27 15:36:48 -0300

    reorder navbar y nuevo contenido

--- content/recursos.R
+++ content/recursos.R
@@ -17,8 +17,8 @@
               "01/2009", # Fecha para ANUARIOS del año previo
               "01/2008", # Fecha para ANUARIOS del año previo
               "01/2007", # DOCUMENTOS TECNICOS
-              "12/2021", "12/2021", "12/2021", "12/2021", "12/2021", "12/2021"
-              ),
+              "12/2021", "12/2021", "12/2021", "12/2021", "12/2021", "12/2021",
+              "06/2022"),
     recurso = c("EOH", # MAYO 2020  -2
                 "Turismo Internacional", # OCTUBRE 2019 9.75
                 "ETI", # ABRIL 2018  9.5
@@ -41,9 +41,10 @@
                 "DT3", # 6
                 "DT4", # 6
                 "DT5", # 4
-                "DT6"), # 2
+                "DT6", # 2
+                "Metodología EVYTH"),
     src = c("img/eoh_metodologico.png",
-            "img/turismo_interacional.png",
+            "img/metodologia_estimacion_tur.png",
             "img/eti_indec.png",
             "img/cuenta_sat.png",
             "img/cuenta_sat_metodo.png",
@@ -64,7 +65,8 @@
             "img/armo03.png",
             "img/armo04.png",
             "img/armo05.png",
-            "img/armo06.png"),
+            "img/armo06.png",
+            "img/metodologia_evyth.png"),
     url = c("https://www.yvera.tur.ar/sinta/informe/documentos/descarga/60c8a5294550f707538011.pdf",
             "https://www.yvera.tur.ar/sinta/informe/documentos/descarga/5dc0460bcfa3e053142696.pdf",
             "https://www.yvera.tur.ar/sinta/informe/documentos/descarga/5d5c00efe6866641938689.pdf","https://www.yvera.tur.ar/sinta/informe/documentos/descarga/61fad7e475abe809073943.pdf",
@@ -86,9 +88,10 @@
             "https://dnme-minturdep.github.io/DT3_registros_adminsitrativos/",
             "https://dnme-minturdep.github.io/DT4_perfiles/",
             "https://dnme-minturdep.github.io/DT5_actividad_empleo/",
-            "https://dnme-minturdep.github.io/DT6_ciencia_de_datos_turismo/"),
+            "https://dnme-minturdep.github.io/DT6_ciencia_de_datos_turismo/",
+            "https://d4t4tur.github.io/evyth_metodologia/"),
     description  = c("Enucesta de Ocupación Hotelera - EOH - Aspectos Metodológicos (mayo de 2020)",
-                     "Estimación del Turismo Internacional de Argentina (octubre de 2019)",
+                     "Estimación del Turismo Internacional de Argentina para todas las vías de acceso - terrestre, fluviames martítimos y aéreos (octubre de 2019).",
                      "Encuesta de Turismo Internacional - ETI (abril de 2018)",
                      "Cuenta satélite de turismo de la Argentina. Medición de la contribución del sector turístico a la economía y sus interrelaciones con el resto de las actividades.  (febrero de 2022)",
                      "Cuenta satélite de turismo de la Argentina. Metodología de estimación. Año base 2004 y serie 2016 - 2019 a precios constantes (febrero de 2022)",
@@ -109,20 +112,22 @@
                      "Documento Técnico #3: Descripción, Análisis y Utilización de los Registros Administrativos para la medición del Turismo",
                      "Documento Técnico #4: Propuestas Metodológicas para las Encuestas de Perfil del Visitante",
                      "Documento Técnico #5: Medición de la contribución económica del turismo",
-                     "Documento Técnico #6: Herramientas Computacionales para el Análisis de Datos"),
+                     "Documento Técnico #6: Herramientas Computacionales para el Análisis de Datos",
+                     "Metodología utilizada en la Encuesta de Viajes y Turismo (EVyTH)."),
     tags = c("Metodología",
              "Metodología",
              "Metodología",
              "Metodología",
              "Metodología",
-             "Documentos de Trabajo; Estadísticas",
-             "Estadísticas",
-             "Documentos de Trabajo; Estadísticas",
-             "Estadísticas","Estadísticas","Estadísticas",
-             "Estadísticas","Estadísticas","Estadísticas",
-             "Estadísticas","Estadísticas","Estadísticas",
-             "Metodología; Armonización", "Metodología; Armonización", "Metodología; Armonización",
-             "Metodología; Armonización", "Metodología; Armonización", "Metodología; Armonización")
+             "Documentos de Trabajo",
+             "Anuario Estadístico",
+             "Documentos de Trabajo",
+             "Anuario Estadístico","Anuario Estadístico","Anuario Estadístico",
+             "Anuario Estadístico","Anuario Estadístico","Anuario Estadístico",
+             "Anuario Estadístico","Anuario Estadístico","Anuario Estadístico",
+             "Metodología", "Metodología", "Metodología",
+             "Metodología", "Metodología", "Metodología",
+             "Metodología")
       ) %>%
     as_tibble() %>%
     mutate(fecha = lubridate::my(fecha)) %>%
