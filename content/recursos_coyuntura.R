ca21914ed3aeabb2706f751fb0c9fac930473e89
Juan Pablo Ruiz Nicolini <jruiznicolini@turismo.gob.ar>
2022-08-05 19:26:46 -0300

    agrego IMET Julio 2022

--- content/recursos_coyuntura.R
+++ content/recursos_coyuntura.R
@@ -4,13 +4,14 @@ library(tidyverse)
 
 data.frame(
   recurso = c("imet_ene2022",
-              "imet_ene2022",
-              "imet_ene2022",
-              "imet_ene2022",
-              "imet_ene2022",
-              "imet_ene2022",
+              "imet_feb2022",
+              "imet_mar2022",
+              "imet_abr2022",
+              "imet_may2022",
+              "imet_jun2022",
               "panorama_may2022",
-              "panorama_jun2022"),
+              "panorama_jun2022",
+              "imet_jul2022"),
   src = c("img/imet.png",
           "img/imet.png",
           "img/imet.png",
@@ -18,7 +19,8 @@ data.frame(
           "img/imet.png",
           "img/imet.png",
           "img/panorama.png",
-          "img/panorama.png"),
+          "img/panorama.png",
+          "img/imet.png"),
   url = c("https://tableros.yvera.tur.ar/recursos/biblioteca/imet_enero.pdf",
           "https://tableros.yvera.tur.ar/recursos/biblioteca/imet_febrero.pdf",
           "https://tableros.yvera.tur.ar/recursos/biblioteca/imet_marzo.pdf",
@@ -26,7 +28,8 @@ data.frame(
           "https://tableros.yvera.tur.ar/recursos/biblioteca/imet_mayo.pdf",
           "https://tableros.yvera.tur.ar/recursos/biblioteca/imet_junio.pdf",
           "https://tableros.yvera.tur.ar/recursos/biblioteca/panorama_mayo.pdf",
-          "https://tableros.yvera.tur.ar/recursos/biblioteca/panorama_junio.pdf"),
+          "https://tableros.yvera.tur.ar/recursos/biblioteca/panorama_junio.pdf",
+          "https://tableros.yvera.tur.ar/institucional/imet/imet_julio_final.pdf"),
   description  = c("Evolución de los principales indicadoresde la actividad turística (ENERO 2022)",
                    "Evolución de los principales indicadoresde la actividad turística (FEBRERO 2022)",
                    "Evolución de los principales indicadoresde la actividad turística (MARZO 2022)",
@@ -34,10 +37,11 @@ data.frame(
                    "Evolución de los principales indicadoresde la actividad turística (MAYO 2022)",
                    "Evolución de los principales indicadoresde la actividad turística (JUNIO 2022)",
                    "Panorama económico del Turismo en Argentina (MAYO 2022)",
-                   "Panorama económico del Turismo en Argentina (JUNIO 2022)"),
+                   "Panorama económico del Turismo en Argentina (JUNIO 2022)",
+                   "Evolución de los principales indicadoresde la actividad turística (JULIO 2022)"),
   tags = c("Estadísticas Turísticas", "Estadísticas Turísticas", "Estadísticas Turísticas", "Estadísticas Turísticas",
-           "Estadísticas Turísticas", "Estadísticas Turísticas", "Economía del Turismo" , "Economía del Turismo"),
-  fecha = c("01/2022","02/2022","03/2022","04/2022","05/2022","06/2022","05/2022","06/2022")
+           "Estadísticas Turísticas", "Estadísticas Turísticas", "Economía del Turismo" , "Economía del Turismo", "Estadísticas Turísticas"),
+  fecha = c("01/2022","02/2022","03/2022","04/2022","05/2022","06/2022","05/2022","06/2022", "07/2022")
 ) %>%
   as_tibble() %>%
   mutate(fecha = lubridate::my(fecha)) %>%
