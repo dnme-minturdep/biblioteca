0190458ebe1b85ff3ae46cedadbcea24111718ad
Juan Pablo Ruiz Nicolini <juanpabloruiznicolini@gmail.com>
2022-09-06 20:48:59 -0300

    agrego funciones para admin recursos de BIBLIOTECA

--- content/update_recursos.R
+++ content/update_recursos.R
@@ -1,4 +1,8 @@
-library(tidyverse)
+
+
+agregar_recurso <- function(){
+
+  library(tidyverse)
 
 googlesheets4::gs4_deauth()
 
@@ -7,3 +11,12 @@ googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1wsgMKiar89XS7
   drop_na() %>%
   write_csv(here::here("content", "recursos.csv"))
 
+}
+
+
+
+abrir_biblioteca <- function(){
+
+  browseURL("https://docs.google.com/spreadsheets/d/1wsgMKiar89XS7XfSMfAistBUYSERVXuyvwxsKzIc1iM/edit#gid=121305671")
+
+}
