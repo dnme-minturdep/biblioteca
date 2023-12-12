6020c7373525ab68ac6c7b76df94098356231f32
Juan Gabriel Juara <jg.juara@gmail.com>
2023-12-12 12:07:20 -0300

    funcion para agregar recursos de ruta natural

--- docs/content/update_recursos.R
+++ docs/content/update_recursos.R
@@ -1,4 +1,9 @@
 
+abrir_biblioteca <- function(){
+
+  browseURL("https://docs.google.com/spreadsheets/d/1wsgMKiar89XS7XfSMfAistBUYSERVXuyvwxsKzIc1iM/edit#gid=121305671")
+
+}
 
 agregar_recurso <- function(){
 
@@ -16,25 +21,44 @@ walk(imgs_path , function(x) {
 
 googlesheets4::gs4_deauth()
 
-googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1wsgMKiar89XS7XfSMfAistBUYSERVXuyvwxsKzIc1iM/edit#gid=121305671") %>%
+googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1wsgMKiar89XS7XfSMfAistBUYSERVXuyvwxsKzIc1iM/edit#gid=121305671", sheet = 1) %>%
   arrange(desc(fecha), desc(recurso)) %>%
   drop_na() %>%
   write_csv(here::here("content", "recursos.csv"))
 
-
-rmarkdown::render(input = "la-ruta-natural.Rmd", output_dir = "docs", output_yaml = "_site.yml")
 
 rmarkdown::render(input = "index.Rmd", output_dir = "docs", output_yaml = "_site.yml")
 
 }
 
 
+agregar_recurso_ruta_natural <- function(){
 
-abrir_biblioteca <- function(){
+  library(tidyverse)
 
-  browseURL("https://docs.google.com/spreadsheets/d/1wsgMKiar89XS7XfSMfAistBUYSERVXuyvwxsKzIc1iM/edit#gid=121305671")
+  imgs_path <- list.files(here::here("img/portadas"),
+                          full.names = T)
+
+  walk(imgs_path , function(x) {
+    print(x)
+    img <- magick::image_read(x)
+    magick::image_resize(img, geometry = "348x207^")
+    magick::image_write(img, x, format = "png")
+  })
+
+  googlesheets4::gs4_deauth()
+
+  googlesheets4::read_sheet("http://docs.google.com/spreadsheets/d/1wsgMKiar89XS7XfSMfAistBUYSERVXuyvwxsKzIc1iM/edit#gid=121305671", sheet = 2) %>%
+    arrange(desc(fecha), desc(recurso)) %>%
+    drop_na() %>%
+    write_csv(here::here("content", "recursos-ruta-natural.csv"))
+
+
+  rmarkdown::render(input = "la-ruta-natural.Rmd", output_dir = "docs", output_yaml = "_site.yml")
 
 }
+
+
 
 
 nueva_publicacion_coyuntura <- function(publicacion = NULL){
